681a9245da6ed75a191f3742848e0d49bc3301c4
Andrey <systerr@gmail.com>
2017-12-09 19:06:00 +0100

    account creating method

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  connection: process.env.ETH_CONNECTION || "ws://localhost:8546"
+  //connection: process.env.ETH_CONNECTION || "ws://localhost:8546"
+  connection: process.env.ETH_CONNECTION || "http://localhost:8545"
 }
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let express = require('express');
 let app = express();
-
+let config = require('./config');
+const Web3 = require('web3');
+web3 = new Web3(config.connection);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('hello world')
+});
+
+app.get('/create', function (req, res) {
+    let account = web3.eth.accounts.create();
+
+    res.json(account);
 });
 
 app.listen(3000);
