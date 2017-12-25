65bc5819971fc526a82caba708b0744ac22fc5b7
EugeneMeduha <mevmail@gmail.com>
2017-12-25 23:52:28 +0300

    index view

--- public/js/main_templ.js
+++ public/js/main_templ.js
@@ -100,3 +100,45 @@ function parallaxScroll(){
 
 });
 
+
+$(document).ready(function(){
+
+
+$('.show_hide').showHide({
+speed: 1000,  // speed you want the toggle to happen
+easing: '',  // the animation effect you want. Remove this line if you dont want an effect and if you haven't included jQuery UI
+changeText: 1, // if you dont want the button text to change, set this to 0
+showText: 'View',// the button text to show when a div is closed
+hideText: 'Close' // the button text to show when a div is open
+
+});
+
+
+});
+
+
+jQuery(document).ready(function( $ ) {
+$('.counter').counterUp({
+delay: 10,
+time: 1000
+});
+});
+
+
+//Hide Overflow of Body on DOM Ready //
+$(document).ready(function(){
+$("body").css("overflow", "hidden");
+});
+
+// Show Overflow of Body when Everything has Loaded
+$(window).load(function(){
+$("body").css("overflow", "visible");
+    var nice=$('html').niceScroll({
+    cursorborder:"5",
+cursorcolor:"#00AFF0",
+cursorwidth:"3px",
+boxzoom:true,
+autohidemode:true
+});
+
+});
